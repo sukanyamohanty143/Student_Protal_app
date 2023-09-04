95cd82c82d8bc1bca15ef57272fc4e2b08727021
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-09-04 10:30:38 +0530

    fix: responsiveness bug fixed

--- studentapp/src/components/BookPub.jsx
+++ studentapp/src/components/BookPub.jsx
@@ -1,71 +1,131 @@
-import React, {useState} from 'react'
-import "./BookPub.css"
+import React, { useState } from "react";
+import "./BookPub.css";
 function BookPub() {
-    const [bookName, setBookName] = useState("");
-    const [bookType, setBookType] = useState("");
-    const [status, setStatus] = useState("");
-    const [publisherName, setPublisherName] = useState("");
-    const [bookDetailsArr, setBookDetailsArr] = useState([])
+  const [bookName, setBookName] = useState("");
+  const [bookType, setBookType] = useState("");
+  const [status, setStatus] = useState("");
+  const [publisherName, setPublisherName] = useState("");
+  const [bookDetailsArr, setBookDetailsArr] = useState([]);
 
-    const AddBookDetails = () => {
-        let newArr = [...bookDetailsArr]
-        newArr.push({ "Bookname": bookName, "Booktype": bookType, "Status": status, "PublisherName": publisherName });
-        setBookDetailsArr(newArr)
-        setBookName("")
-        setBookType("")
-        setStatus("")
-        setPublisherName("")
-    }
+  const AddBookDetails = () => {
+    let newArr = [...bookDetailsArr];
+    newArr.push({
+      Bookname: bookName,
+      Booktype: bookType,
+      Status: status,
+      PublisherName: publisherName,
+    });
+    setBookDetailsArr(newArr);
+    setBookName("");
+    setBookType("");
+    setStatus("");
+    setPublisherName("");
+  };
+
+  return (
+    <>
+      <center>
+        <h2 className="heading">Book Publication</h2>
+      </center>
     
-    return (
-        <div className='main'>
-            <h2 className='heading'>Book Publication</h2>
-            <div className='maincantenerr d-flex justify-content-center'>
-                <div className="">
-                <div className="col-md-5 position-relative textrow" >
-                    <label for="validationTooltip01" className="form-label">Book Name</label>
-                    <input type="text" className="form-control" id="validationTooltip01" placeholder="Enter Book Name" value={bookName} onChange={(e) => setBookName(e.target.value)} required />
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip04" className="form-label">Book Type</label>
-                    <select className="form-select" id="validationTooltip04" required>
-                        <option selected disabled value={bookType} onChange={(e) => setBookType(e.target.value)}>Select</option>
-                        <option value="">...</option>
-                        <option value="">...</option>
-                        <option value="">...</option>
-                    </select>
-                    <div className="invalid-tooltip">
-                        Please select a valid Designation.
-                    </div>
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip04" className="form-label">Status</label>
-                    <select className="form-select" id="validationTooltip04" required>
-                        <option selected disabled value={status} onChange={(e) => setStatus(e.target.value)}>Select</option>
-                        <option value="klvl">klvl</option>
-                        <option value="">...</option>
-                        <option value="">...</option>
-                    </select>
-                    <div className="invalid-tooltip">
-                        Please select a valid Designation.
-                    </div>
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip01" className="form-label">Publisher Name</label>
-                    <input type="text" className="form-control" id="validationTooltip01" value={publisherName} onChange={(e) => setPublisherName(e.target.value)} required />
-                </div><br />
-                <div className="fileupload">
-                    <label for="validationTooltip01" className="form-label">Upload A File</label>
-                    <button className='' type='submit' required>Choose File</button><br/><p>More than 2MB</p>
-                </div>
-                <div className="col-12">
-                    <button className="btn btn-primary" type="submit" onClick={AddBookDetails}>Submit</button>
-                </div>
+    <div className="main maincantener-near">
+      <div className="maincantenerr d-flex justify-content-center">
+        <div className="">
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip01" className="form-label">
+              Book Name
+            </label>
+            <input
+              type="text"
+              id="validationTooltip01"
+              placeholder="Enter Book Name"
+              value={bookName}
+              onChange={(e) => setBookName(e.target.value)}
+              required
+            />
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip04" className="form-label">
+              Book Type
+            </label>
+            <select id="validationTooltip04" required>
+              <option
+                value={bookType}
+                onChange={(e) => setBookType(e.target.value)}
+              >
+                Select
+              </option>
+              <option value="">...</option>
+              <option value="">...</option>
+              <option value="">...</option>
+            </select>
+            <div className="invalid-tooltip">
+              Please select a valid Designation.
+            </div>
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip04" className="form-label">
+              Status
+            </label>
+            <select id="validationTooltip04" required>
+              <option
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}
+              >
+                Select
+              </option>
+              <option value="klvl">klvl</option>
+              <option value="">...</option>
+              <option value="">...</option>
+            </select>
+            <div className="invalid-tooltip">
+              Please select a valid Designation.
+            </div>
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip01" className="form-label">
+              Publisher Name
+            </label>
+            <input
+              type="text"
+              id="validationTooltip01"
+              value={publisherName}
+              onChange={(e) => setPublisherName(e.target.value)}
+              required
+            />
+          </div>
+          <br />
 
+          <div class="custom-file-input">
+            <p>Upload a file</p>
+
+            <div>
+              <label htmlFor="upload-file" className="upload-file-oa button-3">
+                Choose a file
+              </label>
+              <br />
+              <span className="span-text">More than 2MB</span>{" "}
+              <input type="file" id="upload-file" />
             </div>
-            </div>
+          </div>
+
+          <div className="col-12">
+            <button
+              className="btn btn-primary"
+              type="submit"
+              onClick={AddBookDetails}
+            >
+              Submit
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default BookPub
+export default BookPub;
--- studentapp/src/components/ProjectPub.jsx
+++ studentapp/src/components/ProjectPub.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import "./ProjectPub.css";
 
 function ProjectPub() {
@@ -6,65 +6,114 @@ function ProjectPub() {
   const [catagory, setCatagory] = useState("");
   const [submissionDate, setSubmissionDate] = useState("");
   const [status, setStatus] = useState("");
-  const [projectDetailsArr, setProjectDetailsArr] = useState([])
+  const [projectDetailsArr, setProjectDetailsArr] = useState([]);
 
   const AddProjectDetails = () => {
-      let newArr = [...projectDetailsArr]
-      newArr.push({ "Projectname": projectName, "Catagory": catagory, "SubmissionDate": submissionDate, "Status": status });
-      setProjectDetailsArr(newArr)
-      setProjectName("")
-      setCatagory("")
-      setSubmissionDate("")
-      setStatus("")
-  }
+    let newArr = [...projectDetailsArr];
+    newArr.push({
+      Projectname: projectName,
+      Catagory: catagory,
+      SubmissionDate: submissionDate,
+      Status: status,
+    });
+    setProjectDetailsArr(newArr);
+    setProjectName("");
+    setCatagory("");
+    setSubmissionDate("");
+    setStatus("");
+  };
   return (
-    <div>
-          <>
-            <h2 className='heading'>Project Publication</h2>
-            <div className='maincantener'>
-                <div className="col-md-5 position-relative textrow" >
-                    <label for="validationTooltip01" className="form-label">Project Name</label>
-                    <input type="text" className="form-control" id="validationTooltip01" placeholder="Enter Book Name" value={projectName} onChange={(e) => setProjectName(e.target.value)} required />
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip04" className="form-label">Catagory</label>
-                    <select className="form-select" id="validationTooltip04" value={catagory} onChange={(e) => setCatagory(e.target.value)} required>
-                        <option selected disabled value="">Select</option>
-                        <option value="Solo">Solo</option>
-                        <option value="Jount">Jount</option>
-                        <option value="Group">Group</option>
-                    </select>
-                    <div className="invalid-tooltip">
-                        Please select a valid Catagory.
-                    </div>
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip01" className="form-label">Submission Date</label>
-                    <input type="date" className="form-control" id="validationTooltip01" value={submissionDate} onChange={(e) => setSubmissionDate(e.target.value)} required />
-                </div><br />
-                <div className="col-md-5 position-relative textrow">
-                    <label for="validationTooltip04" className="form-label">Status</label>
-                    <select className="form-select" id="validationTooltip04" value={status} onChange={(e) => setStatus(e.target.value)} required>
-                        <option selected disabled value="">Select</option>
-                        <option value="Submited">Submited</option>
-                        <option value="On Going">On Going</option>
-                    </select>
-                    <div className="invalid-tooltip">
-                        Please select a valid Status.
-                    </div>
-                </div><br />
-                <div className="fileupload">
-                    <label for="validationTooltip01" className="form-label">Upload A File</label>
-                    <button className='' type='submit'>Choose File</button><br/><p>More than 2MB</p>
-                </div>
-                <div className="col-12">
-                    <button className="btn btn-primary" type="submit" onClick={AddProjectDetails}>Submit</button>
-                </div>
-
+      <div className="project-publication maincantener-projectpub">
+            <h2 className="heading">Project Publication</h2>
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip01" className="form-label">
+              Project Name
+            </label>
+            <input
+              type="text"
+              id="validationTooltip01"
+              placeholder="Enter Book Name"
+              value={projectName}
+              onChange={(e) => setProjectName(e.target.value)}
+              required
+            />
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip04" className="form-label">
+              Catagory
+            </label>
+            <select
+              
+              id="validationTooltip04"
+              value={catagory}
+              onChange={(e) => setCatagory(e.target.value)}
+              required
+            >
+              <option value="">Select</option>
+              <option value="Solo">Solo</option>
+              <option value="Jount">Jount</option>
+              <option value="Group">Group</option>
+            </select>
+            <div className="invalid-tooltip">
+              Please select a valid Catagory.
             </div>
-        </>
-    </div>
-  )
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip01" className="form-label">
+              Submission Date
+            </label>
+            <input
+              type="date"
+              id="validationTooltip01"
+              value={submissionDate}
+              onChange={(e) => setSubmissionDate(e.target.value)}
+              required
+            />
+          </div>
+          <br />
+          <div className="col-md-5 position-relative textrow">
+            <label htmlFor="validationTooltip04" className="form-label">
+              Status
+            </label>
+            <select
+              
+              id="validationTooltip04"
+              value={status}
+              onChange={(e) => setStatus(e.target.value)}
+              required
+            >
+              <option value="">Select</option>
+              <option value="Submited">Submited</option>
+              <option value="On Going">On Going</option>
+            </select>
+            <div className="invalid-tooltip">Please select a valid Status.</div>
+          </div>
+          <br />
+
+          <div class="custom-file-input">
+            <p>Upload a file</p>
+
+            <div>
+              <label htmlFor="upload-file" className="upload-file-oa button-3">
+                Choose a file
+              </label>
+              <br />
+              <span className="span-text">More than 2MB</span>{" "}
+              <input type="file" id="upload-file" />
+            </div>
+          </div>
+            <button
+              className="btn btn-primary primary-project-submit"
+              type="submit"
+              onClick={AddProjectDetails}
+            >
+              Submit
+            </button>
+        </div>
+    
+  );
 }
 
-export default ProjectPub
+export default ProjectPub;
